59af644500abc976b3886d7e0382e6d87829d766
marionschneider <marion_schl@hotmail.fr>
2016-12-19 01:19:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
 	
-	consumer_key:         'aOI5lXjUsko3dj4gEmf3hbZQO',
-	consumer_secret:      'FLqVjXYIUOSWv1KHEzlXWXITEKNIEaCip8ijV0UAZDrEKWzDhV',
-	access_token:         '787108325473284096-znOSunJKTqxpI0Se9zqJwoHoUwPKkyh',
-  	access_token_secret:  'pGhZQY4gtgWpjdZZgqX0peiIw95vzXNQZTTONxcgJMDdz',	
+	consumer_key:         'YOUR CONSUMER KEY',
+	consumer_secret:      'YOUR COSUMER SECRET KEY',
+	access_token:         'YOUR ACCES TOKEN',
+  	access_token_secret:  'YOUR SECRET ACCES TOKEN',	
 }
